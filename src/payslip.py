d1a460b063bbc35ea5285bb8ad0cff4ef2bbe5f9
rongying <kohrongying@gmail.com>
2021-12-25 18:38:19 +0800

    Update Payslip instance variables.

--- src/payslip.py
+++ src/payslip.py
@@ -6,14 +6,13 @@ class Payslip:
     sheet_name = None
     ws_range = None
 
-    def __init__(self, name, email, sheet_name, wb_range) -> None:
-        self.name = name
-        self.email = email
+    def __init__(self, name, email, sheet_name, ws_range) -> None:
+        self.recipient = PayslipRecipient(name, email)
         self.sheet_name = sheet_name
-        self.ws_range = wb_range
+        self.ws_range = ws_range
 
     def get_recipient(self) -> PayslipRecipient:
-        return PayslipRecipient(self.name, self.email)
+        return self.recipient
 
     def get_ws_range(self):
         return self.ws_range

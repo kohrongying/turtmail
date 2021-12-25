9569a466d1f83413403cbaa5d4087c5a772f7953
rongying <kohrongying@gmail.com>
2021-12-25 18:39:32 +0800

    Refactor main file.

--- main.py
+++ main.py
@@ -1,99 +1,15 @@
-import win32com.client
-from pywintypes import com_error
 import pathlib
 import argparse
-import os
-import sys
 import logging
+from src.email_service import EmailService
+from src.excel_service import ExcelService
+from src.payslip_mailer import PayslipMailer
+from src.payslip_wb_service import PayslipWbService
+from validation_service import is_valid_filepath
 from datetime import date
-import string
-
-import ses
-
-alphabet = list(string.ascii_uppercase)
-
-
-def parse_excel(file_path, to_send_email):
-    WB_PATH = str(pathlib.Path.cwd() / file_path)
-
-    excel = win32com.client.Dispatch("Excel.Application")
-    excel.Visible = False
-
-    try:
-        logging.info(f'Opening filename: {WB_PATH}')
-        wb = excel.Workbooks.Open(WB_PATH)
-
-        for sheet in wb.Sheets:
-            ws = wb.WorkSheets(sheet.Name)
-
-            # Find out used range for the worksheet
-            num_cols = ws.UsedRange.Columns.Count
-            max_row = ws.UsedRange.Rows.Count  # eg. 68
-
-            # max_col = alphabet[num_cols-1] # eg. 'L'
-            max_col = 'L'
-            top_left_cell = 'A1'
-
-            # bottom_right_cell = ws.UsedRange.address.replace('$','').split(':')[-1]
-            bottom_right_cell = 'L95'
-
-            # Find two ranges for two payslips
-            if ws.UsedRange.Find("XX Pte Ltd") is not None:
-                logging.info(f'Worksheet {sheet.Name} belongs to XX')
-                row_split = ws.Range(f'A1:A{max_row}').Find("XX").Row  # 39
-                logging.info(f'Found split at row {row_split}')
-            else:
-                print(f'Worksheet {sheet.Name} does not have XX company name')
-                sys.exit()
-
-            payslip_ranges = [
-                f'{top_left_cell}:{max_col}{row_split - 2}',
-                f'A{row_split}:{bottom_right_cell}'
-            ]
-
-            for rng in payslip_ranges:
-                logging.info(f'Current payslip range is {rng}')
-                payslip = ws.Range(rng)
-                name = payslip.Range('B3').Value
-
-                print(f'name: {name}, range: {rng}')
-
-                email_address = payslip.Range('B4').Value
-                filename = str(pathlib.Path.cwd() / f'files/{name}.pdf')
-                payslip.ExportAsFixedFormat(0, str(pathlib.Path.cwd() / filename))
-                logging.info(f'created: {filename}')
-
-                if to_send_email:
-                    logging.info(f'sending {name}\'s payslip to {email_address}')
-                    ses.send_email(name, email_address, filename)
-                    logging.info('email sent')
-
-        print('\nAll payslips sent')
-
-    except com_error as e:
-        print('Excel failed.')
-        print(e)
-    finally:
-        print('\nClosing excel')
-        wb.Close()
-        excel.Quit()
-
-
-# Check if file_path is valid
-def is_valid_filepath(file_path):
-    file_extension = file_path.split('.')[-1]
-    if not os.path.exists(file_path):
-        print('File does not exist')
-        sys.exit()
-    if file_extension != 'xlsx':
-        print('File has to be of .xlsx type')
-        sys.exit()
-    return True
-
 
 if __name__ == '__main__':
     logging.basicConfig(filename=f'logs/{str(date.today())}.log', level=logging.INFO)
-    logging.info('Started')
 
     # argparse
     parser = argparse.ArgumentParser(description='Job to send out payslips email')
@@ -113,4 +29,18 @@ if __name__ == '__main__':
     logging.info('Checking if filepath is valid')
     is_valid_filepath(arg_file_path)
 
-    parse_excel(arg_file_path, arg_to_send_email)
+    wb = ExcelService.open(arg_file_path)
+    payslips = PayslipWbService(wb).get_payslips()
+    email_service = EmailService()
+
+    for payslip in payslips:
+        name = payslip.recipient.name
+        filename = str(pathlib.Path.cwd() / f'files/{name}.pdf')
+
+        payslip.ws_range.ExportAsFixedFormat(0, str(pathlib.Path.cwd() / filename))
+
+        mailer = PayslipMailer(payslip.recipient, "December", "2020", filename)
+        if arg_to_send_email:
+            email_service.send(mailer)
+            logging.info('email sent')
+

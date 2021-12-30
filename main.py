185f68f250a627fddffdeda3b303fe29b4e34878
kohrongying <kohrongying@gmail.com>
2021-12-30 18:16:43 +0800

    Refactor main.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from src.arg_parser import ArgParser
-from src.payslip_date import PayslipDate
+from src.models.payslip_date import PayslipDate
 from src.services.email_service import EmailService
 from src.services.excel_service import ExcelService
 from src.payslip_mailer import PayslipMailer
@@ -10,24 +10,46 @@ from src.services.logging_service import init_logger
 import logging
 
 
-if __name__ == '__main__':
-    init_logger()
-
-    wb_filepath, to_send_email, payday = ArgParser(sys.argv[1:]).get_args()
-
+def is_input_valid():
     is_valid_filepath(wb_filepath)
-    payslip_date = PayslipDate(payday)
+    PayslipDate(payday)
     logging.info('Input arguments are valid')
 
-    wb = ExcelService().open(wb_filepath)
-    payslips = PayslipWbService(wb).get_payslips()
-    email_service = EmailService()
+
+def get_payslips(wb, payday):
+    payslip_date = PayslipDate(payday)
+    search_terms = ['XX Pte Ltd']
+    return PayslipWbService(wb,
+                            payslip_date=payslip_date,
+                            search_terms=search_terms).get_payslips()
+
+
+def export_payslips(payslips):
     for payslip in payslips:
-        payslip.set_export_directory(f'files/{payslip_date.yearString}/{payslip_date.mthNum}')
         payslip.export_to_pdf()
 
-        if to_send_email:
-            mailer = PayslipMailer(payslip.recipient, payslip_date, payslip.get_abs_filepath())
-            email_service.send(mailer)
+
+def export_and_send_payslips(payslips):
+    email_service = EmailService()
+    sender_email = 'sender@example.com'
+    for payslip in payslips:
+        payslip.export_to_pdf()
+        mailer = PayslipMailer(payslip, sender_email=sender_email)
+        email_service.send(mailer)
+
+
+if __name__ == '__main__':
+    # Set up
+    init_logger()
+    wb_filepath, to_send_email, payday = ArgParser(sys.argv[1:]).get_args()
+    is_input_valid()
+
+    wb = ExcelService().open(wb_filepath)
+    payslips = get_payslips(wb, payday)
+
+    if to_send_email:
+        export_and_send_payslips(payslips)
+    else:
+        export_payslips(payslips)
 
     ExcelService().close(wb)

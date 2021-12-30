c543c7c7a96ac8b05f9a804f0db3b0449cba1019
kohrongying <kohrongying@gmail.com>
2021-12-30 17:21:56 +0800

    Add multiple search terms in wb service.

--- src/payslip_wb_service.py
+++ src/payslip_wb_service.py
@@ -2,6 +2,7 @@ from typing import List
 
 from src.payslip import Payslip
 from src.payslip_ws_service import PayslipWsService
+import logging
 
 
 class PayslipWbService:
@@ -11,8 +12,17 @@ class PayslipWbService:
         self.wb = wb
 
     def get_payslips(self) -> List[Payslip]:
+        search_terms = ['XX Pte Ltd']
         for sheet in self.wb.Sheets:
             ws = self.wb.WorkSheets(sheet.Name)
-            self.payslips.extend(PayslipWsService(ws, sheet, search_terms).get_payslips())
+            if self.is_valid_sheet(sheet):
+                sheet_payslips = PayslipWsService(ws, sheet, search_terms).get_payslips()
+                logging.info(f'Found {len(sheet_payslips)} in {sheet.Name}')
+                self.payslips.extend(sheet_payslips)
+
+        logging.info(f'payslips: {len(self.payslips)}')
         return self.payslips
+
+    def is_valid_sheet(self, sheet):
+        return '&' in sheet.Name
 

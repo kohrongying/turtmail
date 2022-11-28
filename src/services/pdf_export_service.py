8a745407d0d8a75566692a13ab85364452d25183
kohrongying <kohrongying@gmail.com>
2022-11-28 23:33:28 +0800

    Fix pdf export service

--- src/services/pdf_export_service.py
+++ src/services/pdf_export_service.py
@@ -1,8 +1,6 @@
-from dataclasses import dataclass
-from typing import Any
+from typing import List
 
 from src.models.payslip import Payslip
-from src.models.payslip_recipient import PayslipRecipient
 from src.models.payslip_date import PayslipDate
 import pathlib
 import logging
@@ -19,15 +17,15 @@ class PdfExportService:
             self.export_to_pdf(payslip)
 
     def export_to_pdf(self, payslip: Payslip) -> None:
-        abs_filepath = self.get_abs_filepath()
-        payslip.ws_range.ExportAsFixedFormat(0, str(pathlib.Path.cwd() / abs_filepath))
+        abs_filepath = self.get_abs_filepath(payslip)
+        payslip.ws_range.ExportAsFixedFormat(0, abs_filepath)
         logging.info(f"Generated {payslip.recipient.name} payslip at {abs_filepath}")
 
-    def get_abs_filepath(self) -> str:
-        export_directory = self._get_or_create_month_directory()
-        return str(pathlib.Path.cwd() / f"{export_directory}/{self.payslip.recipient.name}.pdf")
+    def get_abs_filepath(self, payslip: Payslip) -> str:
+        export_directory_with_dt = self._get_or_create_export_directory_with_dt()
+        return f"{export_directory_with_dt}/{payslip.recipient.name}.pdf"
 
-    def _get_or_create_month_directory(self) -> str:
+    def _get_or_create_export_directory_with_dt(self) -> str:
         export_directory = f"{self.export_dir}/export/{self.payslip_date.year}/{self.payslip_date.month}"
         pathlib.Path(export_directory).mkdir(parents=True, exist_ok=True)
         return export_directory

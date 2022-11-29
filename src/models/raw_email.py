7946e252814ad12f3abfa0764bd0813be673e211
kohrongying <kohrongying@gmail.com>
2022-11-29 21:51:05 +0800

    Add typing for raw email

--- src/models/raw_email.py
+++ src/models/raw_email.py
@@ -1,15 +1,16 @@
 from abc import ABC, abstractmethod
+from typing import List, Dict
 
 
 class RawEmail(ABC):
     @abstractmethod
-    def source(self):
+    def source(self) -> str:
         pass
 
     @abstractmethod
-    def destinations(self):
+    def destinations(self) -> List[str]:
         pass
 
     @abstractmethod
-    def raw_message(self):
+    def raw_message(self) -> Dict[str, str]:
         pass
